e1034804ca15c6868cf54dd70d59609adb19ba63
Cliff Seal <cliff@logos-creative.com>
2016-05-07 16:23:06 -0400

    Tweak layout and colors

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         },
         uglify: {
             options: {
-                compress: true
+                compress: {}
             },
             build: {
                 src: [
